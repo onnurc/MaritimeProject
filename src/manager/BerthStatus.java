5f9c25d8ba34d8410bad03f460056292f0f88b90
onnurc <110244579+onnurc@users.noreply.github.com>
2025-10-10 13:04:57 +0100

    Update BerthStatus.java

--- src/manager/BerthStatus.java
+++ src/manager/BerthStatus.java
@@ -1,9 +1,5 @@
 package manager;
 
-/*
- * Bu enum, rıhtımın mevcut durumunu belirtir.
- * FREE: Rıhtım boş, OCCUPIED: Gemi yanaşmış (docked)
- */
 public enum BerthStatus {
     FREE,
     OCCUPIED
